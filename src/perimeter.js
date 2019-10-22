0df01a0c149f89b442d7dc10d178c5a93fd95e9a
Ethan Charles <ECharles1@ucvts.tec.nj.us>
2019-10-22 13:17:39 -0400

    added conversion factor and initial value prompts to the file

---
+++ src/perimeter.js
@@ -0,0 +1,5 @@
+const inchToCentimeter = 2.54;
+const widthQuestion = require("readline-sync");
+const paperWidth = widthQuestion.question("Width: ");
+const lengthQuestion = require("readline-sync");
+const paperLength = lengthQuestion.question("Length: ");
