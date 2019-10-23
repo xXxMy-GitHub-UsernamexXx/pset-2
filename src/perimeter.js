9b03e0db6c82acc4ed627dd4705ce9cc509b961d
CRZ <55405534+xXxMy-GitHub-UsernamexXx@users.noreply.github.com>
2019-10-23 10:54:53 -0400

    Update perimeter.js

--- src/perimeter.js
+++ src/perimeter.js
@@ -3,3 +3,9 @@ const widthQuestion = require("readline-sync");
 const paperWidth = widthQuestion.question("Width: ");
 const lengthQuestion = require("readline-sync");
 const paperLength = lengthQuestion.question("Length: ");
+let paperPerimeterInInches = 2 * paperWidth + 2 * paperLength;
+let paperPerimeterInCentimeters = inchToCentimeter * paperPerimeterInInches * 100
+Math.round(paperPerimeterInCentimeters);
+paperPerimeterInCentimeters = paperPerimeterInCentimeters / 100;
+let paperPerimeterInCentimetersWithCommas = paperPerimeterInCentimeters.toLocaleString();
+console.log(paperPerimeterInCentimetersWithCommas);
